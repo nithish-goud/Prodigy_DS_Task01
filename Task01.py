9d52afef64419296a116d5847e14f05486161237
nithish-goud <nithishgoud4376@gmail.com>
2025-09-01 18:56:52 +0530

    Update Task01.py
    “Added Task-01 Data Visualization project (Bar chart & Histogram)”

--- Task01.py
+++ Task01.py
@@ -1,36 +1,34 @@
-#Task -01: Data Visualization
-#Author:Nithish Goud Battupalli
-#Internship:Prodigy Infotech - Data Science
+# Task -01: Data Visualization
+# Author: Nithish Goud Battupalli
+# Internship: Prodigy Infotech - Data Science
 # Objective: Create a bar chart and histogram to visualize data distribution
-#
+
 # 1. Import Required Libraries
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-# For displaying plots in Jupyter Notebook
-%matplotlib inline
-#
-#2. Load Data set
-#
-df = pd.read_csv("C:\Users\Ruchi\Downloads\large_population.csv")
-df.head()
-#
-#3. Bar Chart
+
+# 2. Load Data set
+df = pd.read_csv(r"C:\Users\Ruchi\Downloads\large_population.csv")
+print(df.head())
+
+# 3. Bar Chart - Gender Distribution
 plt.figure(figsize=(6,4))
 sns.countplot(x="Gender", data=df, palette="viridis")
-plt.title("Gender Distribution",fontsize=14)
+plt.title("Gender Distribution", fontsize=14)
 plt.xlabel("Gender")
 plt.ylabel("Count")
 plt.show()
-#
-#4.Histogram
+
+# 4. Histogram - Age Distribution
 plt.figure(figsize=(6,4))
-sns.histplot(df["Age"],bins=10,kde=True,color="blue")
+sns.histplot(df["Age"], bins=10, kde=True, color="blue")
 plt.title("Age Distribution", fontsize=14)
 plt.xlabel("Age")
 plt.ylabel("Frequency")
 plt.show()
-#
-#Summary
-print("Bar Chart shows categorical distribution(Gender).")
-print("Histogram shows continuous distribution(Age).")
+
+# 5. Summary
+print("Bar Chart shows categorical distribution (Gender).")
+print("Histogram shows continuous distribution (Age).")
+
